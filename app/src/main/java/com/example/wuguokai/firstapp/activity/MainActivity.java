d38b421da053239697cacb21812a9be6c089423d
WUGUOKAI <guokai.wu@hand-china.com>
2017-11-17 14:39:49 +0800

    测试了网络请求并获取返回值的

--- app/src/main/java/com/example/wuguokai/firstapp/activity/MainActivity.java
+++ app/src/main/java/com/example/wuguokai/firstapp/activity/MainActivity.java
@@ -4,14 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.wuguokai.firstapp.R;
 import com.example.wuguokai.firstapp.service.TestService;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText ;
@@ -26,12 +33,17 @@ public class MainActivity extends AppCompatActivity {
         editText =(EditText) findViewById(R.id.edit_text);
         final Intent intent = new Intent(this, TestService.class);
 
+        //解决在主线程使用网络的问题
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads().detectDiskWrites().detectNetwork()
+                .penaltyLog().build());
 //        intent.setAction("com.example.wuguokai.firstapp.service.TestService");
 
         Button activityButton = (Button) findViewById(R.id.btn_activity);
         Button serviceButton = (Button) findViewById(R.id.btn_service);
         Button intentButton = (Button) findViewById(R.id.btn_intent);
         Button broadcastButton = (Button) findViewById(R.id.btn_broadcast);
+        Button connectButton= (Button) findViewById(R.id.btn_connect);
         activityButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -71,7 +83,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        connectButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                try{
+                    String urlstr = "http://10.211.98.188:8081/gethello";
+                    URL url = new URL(urlstr);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                    connection.setRequestMethod("POST");
+//                    connection.setRequestProperty("Content-Type", " application/json");//设定 请求格式 json，也可以设定xml格式的
+//                    connection.setRequestProperty("Accept-Charset", "utf-8");  //设置编码语言
+                    connection.connect();
+
+                    if(200 == connection.getResponseCode()){
+                        //得到输入流
+                        InputStream is =connection.getInputStream();
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        byte[] buffer = new byte[1024];
+                        int len = 0;
+                        while(-1 != (len = is.read(buffer))){
+                            baos.write(buffer,0,len);
+                            baos.flush();
+                        }
+                         Toast toast = Toast.makeText(getApplicationContext(), baos.toString(), Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
 
     @Override
     protected void onStart() {
